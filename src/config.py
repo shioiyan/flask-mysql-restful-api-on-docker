86079ed0c3e0691b22ff1d25c19a36ba792189b8
Kanta Matsui <kma9879@gmail.com>
2019-07-01 04:16:28 +0900

    use local mysql

--- src/config.py
+++ src/config.py
@@ -4,15 +4,25 @@ import os
 class DevelopmentConfig:
 
   # SQLAlchemy
+  # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8'.format(
+  #   **{
+  #     'user': os.getenv('DB_USER', 'root'),
+  #     'password': os.getenv('DB_PASSWORD', 'hoge'),
+  #     'host': os.getenv('DB_HOST', 'db'),
+  #     'database': os.getenv('DB_DATABASE', 'hoge'),
+  #   })
+
+  # SQLAlchemy
   SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8'.format(
     **{
       'user': os.getenv('DB_USER', 'root'),
-      'password': os.getenv('DB_PASSWORD', 'hoge'),
-      'host': os.getenv('DB_HOST', 'db'),
+      'password': os.getenv('DB_PASSWORD', ''),
+      'host': os.getenv('DB_HOST', 'docker.for.mac.localhost'),
       'database': os.getenv('DB_DATABASE', 'hoge'),
     })
+
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   SQLALCHEMY_ECHO = False
 
-
 Config = DevelopmentConfig
+
